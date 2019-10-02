05f184ec8f196b7e9bee4b763b33f6874944b8d9
darinamandarina <darinamandarina97@gmail.com>
2019-10-02 09:11:31 +0200

    start of the proj

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ class App extends React.Component {
   
   render(){
     return <div className="App">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 <PurchaseHistory/>
             </div>
   
