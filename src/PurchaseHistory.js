ee5faa063d5ced656824a78362eaddf7855b7d01
yoleo <yoleo@yandex.ru>
2019-10-02 09:08:53 +0200

    start of the proj

---
+++ src/PurchaseHistory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// material-ui components and methods
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
+class PurchaseHistory extends React.Component {
+  
+    render(){
+      return(
+
+      );
+    
+    }
+  }
+  
+  export default PurchaseHistory;
