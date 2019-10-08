91cb74924e3e13a1ed2a05fcf1f795259ac01758
Паирель Дарина Яковлевна <140736@herzen.spb.ru>
2019-10-08 14:54:44 +0300

    Updates

---
+++ precache-manifest.34bbe2d347afe394ca7d375f345e6078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad12f15d97e0cbf097ca413644ac6c0",
+    "url": "/b-proj-test/index.html"
+  },
+  {
+    "revision": "f98e69a115044ebfd8a4",
+    "url": "/b-proj-test/static/css/main.b079bffd.chunk.css"
+  },
+  {
+    "revision": "1862d7547d51b2ca8531",
+    "url": "/b-proj-test/static/js/2.0364315c.chunk.js"
+  },
+  {
+    "revision": "f98e69a115044ebfd8a4",
+    "url": "/b-proj-test/static/js/main.f1d14b14.chunk.js"
+  },
+  {
+    "revision": "a2270ef08848227ae9a9",
+    "url": "/b-proj-test/static/js/runtime-main.e31f47e5.js"
+  }
+]);
